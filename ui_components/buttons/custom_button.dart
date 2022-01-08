4b3f581a096d8621083bc27941e81c5ce028fd68
AJAY KUMAR.V <ajaykumar.v@AJAYs-MacBook-Pro.local>
2022-01-08 18:35:14 +0530

    Added labels common UI

--- ui_components/buttons/custom_button.dart
+++ ui_components/buttons/custom_button.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:vinvestor/vui/colors/colors.dart';
 
 class CustomButton extends StatelessWidget {
   String title;
   VoidCallback onCustomButtonPressed;
-  Color backgroundColor;
+  Color btnBackgroundColor;
   Color btnTextColor;
-  CustomButton(this.title, this.onCustomButtonPressed, this.backgroundColor, this.btnTextColor,  {Key? key}) : super(key: key);
+  CustomButton(this.title, this.onCustomButtonPressed, this.btnBackgroundColor, this.btnTextColor,  {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onCustomButtonPressed,
-      child: Text(title, style: const TextStyle(color: AppColors.colorSeqBlueOne, fontSize: 20),),
+      child: Text(title, style: TextStyle(color: btnTextColor),),
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 20),
+        primary: btnBackgroundColor,
+      ),
     );
   }
 }
